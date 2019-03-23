71b6ef06feea7c74ff27e9b5b0662481f42456e5
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2019-03-23 10:30:14 +0530

    init

---
+++ services/superhero.js
@@ -0,0 +1,18 @@
+
+
+// const rp = require('request-promise');
+// const config = require('config/config.json')
+// export class SuperheroService {
+//     search(key) {
+//         return new Promise((resolve, reject) => {
+//             rp(`${config.url}/${config.apiKey}/search/${key}`)
+//                 .then(function (htmlString) {
+//                     // Process html...
+//                 })
+//                 .catch(function (err) {
+//                     // Crawling failed...
+//                 });
+//         });
+
+//     }
+// }
